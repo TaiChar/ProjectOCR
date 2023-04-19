d30611ff69df36029463d092e159da9989af1d3a
TaiChar <129651620+TaiChar@users.noreply.github.com>
2023-04-19 12:19:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,18 @@ import cv2 as cv
 from PIL import Image 
 import pandas as pd
 
+# Read in data from the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
+
+df = load_data(st.secrets["public_gsheets_url"])
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.PORQ No.} :{row.Total}:")
+    
 #def extract_total_amount_due(output):
 #    pattern = r'Total\s+amount\s+due\s+\(USD\)\s+([\d.]+)'
 #    match = re.search(pattern, output)
